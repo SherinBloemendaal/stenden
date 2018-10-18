e1458d2c79d359d4f65e403dc205fb26cdb07b26
Sherin Bloemendaal <sherin@blueberry.nl>
2018-10-18 14:08:32 +0200

    Exampol button

--- index.php
+++ index.php
@@ -21,6 +21,10 @@
 
 <body>
 
+<div class="container">
+    <button class="btn btn-primary">IK WIL DOOD</button>
+</div>
+
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
